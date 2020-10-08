e13bb371362d66e2b4601a7028aa9b7ce6057180
Manishkvivek <71657998+Manishkvivek@users.noreply.github.com>
2020-10-08 19:40:54 +0530

    Add files via upload

---
+++ .gitignore/MaxOfIntegers.java
@@ -0,0 +1,21 @@
+
+package src.codewithvivek;
+
+public class MaxOfIntegers {
+	public static void main(String[] args) {
+
+	     double first = 45.46;
+	     double second = 68.48;
+	     double third = 53.81;
+	     
+	     if ( first >= second && first>= third) {
+	     System.out.println( first+ " is the largest no.");
+	     }
+	      if ( second>= first && second >= third) {
+	     System.out.println(second+ " is the largest no.");
+	      }
+	     else {
+	     System.out.println(third + " is the largest no."); }
+	     
+}
+}
